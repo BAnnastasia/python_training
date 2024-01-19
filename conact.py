3a858da6dc358fc89adbfb2b976369a5a65a699a
Anastasia Borodich <>
2024-01-19 14:10:13 +0300

    H1_Ex3_v1

---
+++ conact.py
@@ -0,0 +1,45 @@
+
+class Contact:
+
+    def __init__(self, firstname, lastname, address, mobile, email, byear, bmonth, bday, homepage,
+                 #middlename,
+                 #nickname, photo, delete,
+                 #company, title,
+                 #home,
+                 #work, fax,
+                 #email2, email3,
+                 #aday, amonth, ayear,
+                 #new_group, address2, phone2, notes
+                 ):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.byear = byear
+        self.bmonth = bmonth
+        self.bday = bday
+        self.homepage = homepage
+        #self.middlename = middlename
+        #self.nickname = nickname
+        #self.photo = photo
+        #self.delete = delete
+        #self.company = company
+        #self.title = title
+        #self.home = home
+        #self.work = work
+        #self.fax = fax
+        #self.email2 = email2
+        #self.email3 = email3
+        #self.aday = aday
+        #self.amonth = amonth
+        #self.ayear = ayear
+        #self.new_group = new_group
+        #self.address2 = address2
+        #self.phone2 = phone2
+        #self.notes = notes
+
+
+
+
+
