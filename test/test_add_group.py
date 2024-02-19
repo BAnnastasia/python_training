2b2d0dac851e39a0d550f25779791caf8effb1d3
Anastasia Borodich <>
2024-02-19 15:06:25 +0300

    H6_Ex

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,14 @@
-import pytest
+
 import allure
 from model.group import Group
-from data.add_group import constant as data_test
 
 
-@pytest.mark.parametrize('group', data_test, ids=[repr(x) for x in data_test])
+
+
 @allure.description("This test successfully creates a group")
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
-    #group = Group(name, header, footer)
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
